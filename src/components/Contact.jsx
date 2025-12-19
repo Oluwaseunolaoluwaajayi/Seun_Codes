175b4f36d6ae2bc9fd8cbb86c67816482385535e
Xlarge4u <ajayiolaoluwa8@gmail.com>
2025-12-19 14:38:46 +0100

    Update contact form with Web3Forms integration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,7 @@ export default function Contact() {
 
             <div className="contact-details">
               <p>
-                <FiMail className="icon" /> ajayiolaoluwa88@yahoo.com
+                <FiMail className="icon" /> ajayiolaoluwa8@gmail.com
               </p>
               <p>
                 <FiPhone className="icon" /> +234 805 626 6468
@@ -64,14 +64,34 @@ export default function Contact() {
           {/* Right: Contact Form */}
           <form
             className="contact-form"
-            action="https://formsubmit.co/ajayiolaoluwa88@yahoo.com"
+            action="https://api.web3forms.com/submit"
             method="POST"
           >
-            <input type="hidden" name="_captcha" value="false" />
-            <input type="hidden" name="_next" value="https://yourdomain.com/thanks" /> {/* optional */}
+            {/* 🔑 Web3Forms Access Key */}
+            <input
+              type="hidden"
+              name="access_key"
+              value="a910d809-1b19-4f32-a626-bac60254d55e"
+            />
 
-            <input type="text" name="name" placeholder="Your Name" required />
-            <input type="email" name="email" placeholder="Your Email" required />
+            {/* Optional Settings */}
+            <input type="hidden" name="subject" value="New Portfolio Message" />
+            <input type="hidden" name="from_name" value="Seun Codes Portfolio" />
+            <input type="hidden" name="redirect" value="https://yourdomain.com/thanks" />
+            <input type="hidden" name="botcheck" />
+
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
             <textarea
               name="message"
               rows="6"
